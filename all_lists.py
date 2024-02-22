f39c9768edbeb61b266078884101cb1023afee47
Mehmet <98655470+mehmetblue@users.noreply.github.com>
2024-02-23 00:59:48 +0100

    Add files via upload
    all_lists.py added

---
+++ all_lists.py
@@ -0,0 +1,52 @@
+Country_list = ['Afghanistan',  'Albania', 'Algeria', 'Andorra', 'Angola', 'Antigua and Barbuda', 'Argentina', 'Armenia', 'Australia', 'Austria', 'Azerbaijan',
+                'Bahamas', 'Bahrain', 'Bangladesh', 'Barbados', 'Belarus', 'Belgium', 'Belize', 'Benin', 'Bhutan', 'Bolivia', 'Bosnia and Herzegovina', 'Botswana',
+                'Brazil', 'Brunei Darussalam', 'Bulgaria', 'Burkina Faso', 'Burundi', 'Cambodia', 'Cameroon', 'Canada', 'Cape Verde', 'Central African Republic',
+                'Chile', 'China', 'Colombia', 'Congo, Republic of the...', 'Costa Rica', 'Croatia', 'Cuba', 'Cyprus', 'Czech Republic', "Côte d'Ivoire",
+                "Democratic People's Republic of Korea", 'Democratic Republic of the Congo', 'Denmark', 'Djibouti', 'Dominica', 'Dominican Republic', 'Ecuador',
+                'Egypt', 'El Salvador', 'Eritrea', 'Estonia', 'Ethiopia', 'Fiji', 'Finland', 'France', 'Gabon', 'Gambia', 'Georgia', 'Germany', 'Ghana', 'Greece',
+                'Grenada', 'Guatemala', 'Guinea', 'Guinea-Bissau', 'Guyana', 'Haiti', 'Honduras', 'Hong Kong (S.A.R.)', 'Hungary', 'Iceland', 'India', 'Indonesia',
+                'Iran, Islamic Republic of...', 'Iraq', 'Ireland', 'Israel', 'Italy', 'Jamaica', 'Japan', 'Jordan', 'Kazakhstan', 'Kenya', 'Kuwait', 'Kyrgyzstan',
+                'Latvia', 'Lebanon', 'Lesotho', 'Liberia', 'Libyan Arab Jamahiriya', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Madagascar', 'Malawi', 'Malaysia',
+                'Maldives', 'Mali', 'Malta', 'Marshall Islands', 'Mauritania', 'Mauritius', 'Mexico', 'Micronesia, Federated States of...', 'Monaco', 'Mongolia',
+                'Montenegro', 'Morocco', 'Mozambique', 'Myanmar', 'Namibia', 'Nauru', 'Nepal', 'Netherlands', 'New Zealand', 'Nicaragua', 'Niger', 'Nigeria',
+                'North Korea', 'Norway', 'Oman', 'Other Country (Not Listed Above)', 'Pakistan', 'Palau', 'Panama', 'Paraguay', 'Peru', 'Philippines', 'Poland',
+                'Portugal', 'Qatar', 'Republic of Korea', 'Republic of Moldova', 'Romania', 'Russian Federation', 'Rwanda', 'Saint Lucia', 'San Marino',
+                'Saudi Arabia', 'Senegal', 'Serbia', 'Sierra Leone', 'Singapore', 'Slovakia', 'Slovenia', 'Solomon Islands', 'Somalia', 'South Africa',
+                'South Korea', 'Spain', 'Sri Lanka', 'Sudan', 'Suriname', 'Swaziland', 'Sweden', 'Switzerland', 'Syrian Arab Republic', 'Taiwan', 'Tajikistan',
+                'Thailand', 'The former Yugoslav Republic of Macedonia', 'Timor-Leste', 'Togo', 'Trinidad and Tobago', 'Tunisia', 'Turkey', 'Turkmenistan',
+                'Uganda', 'Ukraine', 'United Arab Emirates', 'United Kingdom', 'United Republic of Tanzania', 'United States', 'Uruguay', 'Uzbekistan',
+                'Venezuela, Bolivarian Republic of...', 'Viet Nam', 'Yemen', 'Zambia', 'Zimbabwe']
+
+YearsCodingProf_list = ["0-2 years", "3-5 years", "6-8 years", "9-11 years", "12-14 years", "15-17 years", 
+                        "18-20 years", "21-23 years", "24-26 years", "27-29 years", "30 or more years"]
+
+RaceEthnicity_list = ["White or of European descent", "South Asian", "Hispanic or Latino/Latina", "East Asian", "Middle Eastern",
+                        "Black or of African descent", "Native American, Pacific Islander, or Indigenous Australian"]
+
+Age_list = ["Under 18 years old", "18 - 24 years old", "25 - 34 years old", "35 - 44 years old", "45 - 54 years old", "55 - 64 years old", "65 years or older"]
+
+Employment_list = ["Employed full-time", "Independent contractor, freelancer, or self-employed", "Not employed, but looking for work",
+                    "Employed part-time", "Not employed, and not looking for work", "Retired"]
+
+FormalEducation_list = ["I never completed any formal education", "Primary/elementary school", "Secondary school", 
+                        "Some college/university study without earning a degree", "Associate degree", "Bachelor's degree",
+                        "Master's degree", "Professional degree", "Doctoral degree"]
+
+CommunicationTools_list = ["Office / productivity suite (Microsoft Office, Google Suite, etc.)", "Slack ", "Confluence ", "Jira "
+                            "Other chat system (IRC, proprietary software, etc.) ", "Other wiki tool (Github, Google Sites, proprietary software, etc.)",
+                            "Trello ", "Google Hangouts/Chat ", "HipChat ", "Facebook ", "Stack Overflow Enterprise"]
+
+NumberMonitors_list = ["1", "2", "3"," 4", "More than 4"]
+
+DevType_list = ["Back-end developer", "Full-stack developer", "Front-end developer", "Mobile developer", "Desktop or enterprise applications developer",
+                "Student", "Database administrator", "Designer", "System administrator", "DevOps specialist", "Data or business analyst",
+                "Data scientist or machine learning specialist", "QA or test developer", "Engineering manager", "Embedded applications or devices developer",
+                "Game or graphics developer", "Product manager", "Educator or academic researcher", "C-suite executive (CEO, CTO, etc.)", 
+                "Marketing or sales professional"]
+
+LastNewJob_list = ["I've never had a job", "Less than a year ago", "Between 1 and 2 years ago", "Between 2 and 4 years ago", "More than 4 years ago"]
+
+Student_list = ["No", "Yes, part-time", "Yes, full-time"]
+
+ide_list = ["Visual Studio Code", "Visual Studio", "Notepad++", "Sublime Text", "Vim", "IntelliJ", "Android Studio", "Eclipse", "Atom", "PyCharm",
+            "Xcode", "PHPStorm", "NetBeans", "IPython / Jupyter", "Emacs", "RStudio", "RubyMine", "TextMate", "Coda", "Komodo", "Zend", "Light Table"]
